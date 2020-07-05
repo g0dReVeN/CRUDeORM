baaf7d093f8afab15af0af2281bd10c71ef82b7c
g0dReVeN <jgovend@student.wethinkcode.co.za>
2020-07-05 22:47:51 +0200

    separated custom model creation logic from index method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { pool } from "./connection";
-import Model from "./model";
+import customModel from "./customModel";
 import schema from "./schema";
 
 export default class CRUDe {
@@ -13,47 +13,19 @@ export default class CRUDe {
 		this.conn = pool(dbDetails);
 	}
 
-	async end() {
-		await this.conn.end();
-	}
-
 	execute(params) {
 		return this.conn.connect(params);
+	}
+
+	async end() {
+		await this.conn.end();
 	}
 
 	async createSchema(table, columns) {
 		return await schema(table, columns, this);
 	}
 
-	async createModel(modelName, tableName) {
-		const crude = this;
-
-		if (typeof tableName === "string" && typeof modelName === "string") {
-			const modelClass = class extends Model {
-				constructor(fields) {
-					super(tableName, crude);
-
-					return this.insert(fields);
-				}
-
-				static get table() {
-					return tableName;
-				}
-
-				static get conn() {
-					return crude.conn;
-				}
-
-				static get debug() {
-					return crude.debug;
-				}
-			};
-
-			Object.defineProperty(modelClass, "name", { value: modelName });
-
-			return modelClass;
-		}
-
-		return null;
+	createModel(modelName, tableName) {
+		return customModel(tableName, modelName, this)
 	}
 }
